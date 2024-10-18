835101c59eb90eb41932aaf705d660ac14d30f0e
kishanhitk <kishansharma1231@gmail.com>
2024-10-18 16:43:07 +0530

    feat: add shadcn ui

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { Button } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [
@@ -49,6 +50,7 @@ export default function Index() {
           </ul>
         </nav>
       </div>
+      <Button>Hello</Button>
     </div>
   );
 }
