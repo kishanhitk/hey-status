40ad36cb990ca36e9283590f6c552168e593d299
kishanhitk <kishansharma1231@gmail.com>
2024-10-19 14:21:09 +0530

    clean: dashboard page

--- app/routes/dashboard/route.tsx
+++ app/routes/dashboard/route.tsx
@@ -5,14 +5,10 @@ import { SidebarInset, SidebarProvider } from "~/components/ui/sidebar";
 export default function DashboardLayout() {
   return (
     <SidebarProvider>
-      <div className="flex h-screen">
-        <AppSidebar />
-        <main className="flex-1 overflow-y-auto">
-          <SidebarInset>
-            <Outlet />
-          </SidebarInset>
-        </main>
-      </div>
+      <AppSidebar />
+      <SidebarInset>
+        <Outlet />
+      </SidebarInset>
     </SidebarProvider>
   );
 }
