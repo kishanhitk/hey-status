c06c1dc648939c4f4917133d477d6f16c86cb5c8
kishanhitk <kishansharma1231@gmail.com>
2024-10-19 12:41:02 +0530

    feat: demo homepage

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,9 @@ export default {
   content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      container: {
+        center: true,
+      },
       fontFamily: {
         sans: [
           "Inter",
