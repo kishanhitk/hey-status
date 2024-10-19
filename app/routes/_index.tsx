b70513a43de5cb1112f7733deaba52f898c3a3ee
kishanhitk <kishansharma1231@gmail.com>
2024-10-19 12:42:48 +0530

    feat: demo homepage

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -6,8 +6,8 @@ import { Input } from "~/components/ui/input";
 
 export default function Component() {
   return (
-    <div className="flex flex-col min-h-screen container">
-      <header className="px-4 lg:px-6 h-14 flex items-center">
+    <div className="flex flex-col min-h-screen ">
+      <header className="px-4 lg:px-6 h-14 flex items-center container">
         <Link className="flex items-center justify-center" to="#">
           <Globe className="h-6 w-6" />
           <span className="ml-2 text-xl font-bold">OpenStatus</span>
