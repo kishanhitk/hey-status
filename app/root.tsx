10c56be45b634c0f6b01c13e6c32ee9b224babfd
kishanhitk <kishansharma1231@gmail.com>
2024-10-20 18:34:21 +0530

    feat: add nprogress

--- app/root.tsx
+++ app/root.tsx
@@ -6,13 +6,17 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useFetchers,
   useLoaderData,
+  useNavigation,
   useRevalidator,
 } from "@remix-run/react";
 
 import styles from "./tailwind.css?url";
+import NProgress from "nprogress";
+import nProgressStyles from "./nprogress.css?url";
 import { createServerSupabase } from "./utils/supabase.server";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { useSupabase } from "./hooks/useSupabase";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "~/components/ui/toaster";
@@ -47,6 +51,8 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
+  { rel: "stylesheet", href: nProgressStyles },
+
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
     rel: "preconnect",
@@ -102,6 +108,34 @@ export default function App() {
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [serverAccessToken, supabase]);
+
+  const transition = useNavigation();
+
+  const fetchers = useFetchers();
+  /**
+   * This gets the state of every fetcher active on the app and combine it with
+   * the state of the global transition (Link and Form), then use them to
+   * determine if the app is idle or if it's loading.
+   * Here we consider both loading and submitting as loading.
+   */
+  const state = useMemo<"idle" | "loading">(
+    function getGlobalState() {
+      const states = [
+        transition.state,
+        ...fetchers.map((fetcher) => fetcher.state),
+      ];
+      if (states.every((state) => state === "idle")) return "idle";
+      return "loading";
+    },
+    [transition.state, fetchers]
+  );
+  useEffect(() => {
+    // and when it's something else it means it's either submitting a form or
+    // waiting for the loaders of the next location so we start it
+    if (state === "loading") NProgress.start();
+    // when the state is idle then we can to complete the progress bar
+    if (state === "idle") NProgress.done();
+  }, [state, transition.state]);
 
   return (
     <QueryClientProvider client={queryClient}>
